6f2849035022439346bcaef188c5d797da3b8daa
Dinda Rachmayanti <dindarachmayanti3@gmail.com>
2021-09-15 14:21:16 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,30 @@
+<?php  
+$nama = "Dinda Rachmayanti";
+$kelas = "MI20B";
+$jurusan = "Manajemen Informatika";
+?>
+<DOCTYPE html>
+<html>
+<head>
+		<title>Week 1 - Dinda Rachmayanti</title>
+<head>
+<body>
+		<?php
+			echo "Hallo World";
+			echo "<br>"; //untuk memberikan spasi
+			echo $nama;
+			echo "<br>";
+			echo "Hallo ".$nama;
+			echo "<br>";
+			echo "<br>";
+			echo "BIODATA";
+			echo "<br>";
+			echo "<br>";
+			echo "Nama : ".$nama;
+			echo "<br>";
+			echo "Kelas : ".$kelas;
+			echo "<br>";
+			echo "Jurusan : ".$jurusan;
+		?>
+</body>
+</html>
