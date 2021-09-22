3c8d03a612f43f15e1a195ce67cc24abda7bfbc2
Dinda Rachmayanti <dindarachmayanti3@gmail.com>
2021-09-22 10:21:25 +0700

    Create array.php

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php 
+ 
+//penamaan isi array variabel biodata
+$biodata = array(
+	'nama' => "Dinda Rachmayanti",
+	'kelas' => "MI20B",
+	'jurusan' => "Manajemen Informatika",
+	
+);
+ 
+// menampilkan isi array yang bernama nama
+echo $biodata['nama'];
+ 
+?>
