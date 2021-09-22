00fbaa33705e773c239e5e45737a11244a25e1b7
Dinda Rachmayanti <dindarachmayanti3@gmail.com>
2021-09-22 12:24:05 +0700

    Create week2.php

---
+++ week2.php
@@ -0,0 +1,32 @@
+<?php  
+$nama = "Dinda Rachmayanti";
+$alamat = "Tasikmalaya";
+$no_hp = "089516203289";
+$matkul = array('Web Programming','System Design Analysis','Java Programming');
+?>
+<DOCTYPE html>
+<html>
+<head>
+		<title>Week 2 - Dinda Rachmayanti</title>
+<head>
+<body>
+		<?php
+			echo "~~~~~~~~~~~~~";
+			echo "BIODATA";
+			echo "~~~~~~~~~~~~~";
+			echo "<br>";
+			echo "<br>";
+			echo "Nama : ".$nama;
+			echo "<br>";
+			echo "Alamat : ".$alamat;
+			echo "<br>";
+			echo "NO HP : ".$no_hp; 
+			echo "<br>";
+			echo "Saya Mengambil mata kuliah sebagai berikut : ";
+			echo "<br>";
+			for($x=0;$x<count($matkul);$x++){
+			echo "$x." .$matkul[$x]."<br/>";
+}
+		?>
+</body>
+</html>
