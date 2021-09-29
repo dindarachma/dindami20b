27f98994f33cc1af63c79c63c971796dd169f62b
Dinda Rachmayanti <dindarachmayanti3@gmail.com>
2021-09-29 14:14:50 +0700

    Create index.php

---
+++ week3/index.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(1);
+ 
+//DATABASE PERTAMA
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$database = 'db_suratdinda';
+ 
+// Buat Koneksinya
+$db = new mysqli($db_host, $db_user, $db_pass, $database);
+ 
+// Cek koneksi *BISA SOBAT HAPUS NANTINYA 
+if ($db->connect_error) { 
+	die("<b>Yahh! Koneksi Database pertama 'db_suratdinda' gagal</b> : " . mysqli_connect_error()); 
+} 
+else { 
+	echo "<b>Hore! Koneksi Database pertama 'db_suratdinda' Berhasil</b>"; 
+}
+
+?>
