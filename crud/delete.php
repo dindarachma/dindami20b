6735038ac9dc4d7ab0ac5905f96126b8ac052b2a
Dinda Rachmayanti <dindarachmayanti3@gmail.com>
2021-10-28 19:26:56 +0700

    Update delete.php

--- crud/delete.php
+++ crud/delete.php
@@ -9,7 +9,7 @@ $con = new mysqli("localhost","root", "","db_suratdinda");
       //Insert user data info table
       $result = mysqli_query($con, "DELETE FROM `tbl_surat` WHERE `tbl_surat`.`id` = $id" ); 
 
-      header("Location:view.php");
+      header("Location:view.php?pesan=success&&frm=del");
     }
 
   ?>
